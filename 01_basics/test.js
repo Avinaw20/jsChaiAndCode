471d84ee763ac70bc5c23417e7419b0476c08922
Avinaw20 <96725378+Avinaw20@users.noreply.github.com>
2025-09-28 05:19:47 +0000

    tricky consoles

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("hello buddy")
+console.log(undefined >= 0)
+console.log(null >= 0)
+console.log("Hey")
+console.log("2">1)
+console.log("02">1)
+// aisa iss bcoz null -> 0 || NaN
+// kis tarah se data ko memory me rakha jaata hai aur access kiya jaata hai uss basis par data ke do types hote hai -- primitive &  non-primitive
+
+
